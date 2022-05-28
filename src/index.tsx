0e78be89e96a2deadbec3112ddb36903b8330492
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-05-28 08:51:01 +0430

    feat(provider): add provider for future golobal state

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,14 @@ import { render } from "solid-js/web";
 
 import "./index.css";
 import App from "./App";
+import { Provider } from "./store/";
+
+function Application() {
+  return (
+    <Provider>
+      <App />
+    </Provider>
+  );
+}
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
