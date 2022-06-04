4500d1eff813456346a092ed6913b6d4618ba4f7
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-04 22:55:47 +0430

    feat: add backlogs resource and actions

--- src/store/createBacklogs.ts
+++ src/store/createBacklogs.ts
@@ -3,31 +3,97 @@ import { createResource } from "solid-js";
 import type { Resource } from "solid-js";
 import { SetStoreFunction } from "solid-js/store";
 import { StoreType } from "../types/store";
-import { PomodoroType } from "../types/pomodoro";
-import { fetchBacklogs } from "../api/backlogs";
+import { BacklogType, CreateDto, PomodoroType } from "../types/pomodoro";
+import { fetchBacklogs, addBacklog } from "../api/backlogs";
+import { changeOrder, removePomodoro } from "../api/shared";
+import { Actions } from ".";
+import { ChangeOrderDto } from "../types/shared";
+
+export interface BacklogActions {
+  //
+}
 
 // TODO: remove never
 export default function createBacklogs(
   actions: any,
   state: StoreType,
   setState: SetStoreFunction<StoreType>
-): Resource<PomodoroType[]> {
-  const [backlogs, { mutate: backlogsMutate, refetch: refetchBacklogs }] =
-    createResource<PomodoroType[]>(
-      async () => await fetchBacklogs(state.token),
-      {
-        initialValue: [],
-      }
-    );
+): Resource<BacklogType[]> {
+  const [backlogs, { mutate, refetch }] = createResource<BacklogType[]>(
+    async () => await fetchBacklogs(state.token),
+    {
+      initialValue: [],
+    }
+  );
 
-  Object.assign(actions, {
+  Object.assign<Actions, BacklogActions>(actions, {
     loadBacklogs() {
-      return refetchBacklogs();
+      return refetch();
     },
-    mutateBacklogs(data: PomodoroType[]) {
-      return backlogsMutate(data);
+    mutateBacklogs(data: BacklogType[]) {
+      return mutate(data);
     },
-    addBacklog() {},
+    addBacklog(data: CreateDto) {
+      return addBacklog(data, state.token);
+    },
+
+    addPendingItem(data: { title: string; creationTime: string }) {
+      mutate((prev) => [
+        ...prev,
+        {
+          current: 0,
+          done: false,
+          end: 0,
+          isRemoved: false,
+          status: "Backlog",
+          title: data.title,
+          updated_at: data.creationTime,
+          created_at: data.creationTime,
+          order: prev.length + 1,
+          _id: "Pending",
+        },
+      ]);
+    },
+
+    removePendingItem(creationTime: string) {
+      mutate((prev) => prev.filter((item) => item.created_at !== creationTime));
+    },
+
+    revalidateAddedItem(addedTask: any, creationTime: string) {
+      mutate((prev) =>
+        prev.map((item) => {
+          if (item.created_at === creationTime) {
+            return addedTask;
+          }
+          return item;
+        })
+      );
+    },
+
+    remove(id: string) {
+      return removePomodoro(id, state.token);
+    },
+
+    changeOrder(data: ChangeOrderDto) {
+      return changeOrder(data, state.token);
+    },
+
+    moveBacklogItemAndReOrder(id: string) {
+      mutate((prev) => {
+        return prev
+          ?.filter((item) => item._id !== id)
+          .map((item, order) => ({ ...item, order: order + 1 }));
+      });
+    },
+
+    addMovedPomodoroItem(movedItem: PomodoroType) {
+      mutate((prev) => {
+        return [
+          ...prev,
+          { ...movedItem, status: "Backlog", order: prev.length },
+        ];
+      });
+    },
   });
 
   return backlogs;
