b7c4780b40d508678e79cc8eb98dcaf7f6e62d5d
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-04 22:55:25 +0430

    feat: add pomodoros resource and actions

--- src/store/createPomodoro.ts
+++ src/store/createPomodoro.ts
@@ -1,1 +1,66 @@
-//import { PomodoroType } from "../types/pomodoro";
+import { createSignal } from "solid-js";
+import { SetStoreFunction } from "solid-js/store";
+import { StoreType } from "../types/store";
+import { PomodoroFocusType, PomodoroType } from "../types/pomodoro";
+import { Actions } from ".";
+import { editPomodoro } from "../api/pomodoro";
+
+// TODO
+export interface PomodoroActions {
+  //
+}
+
+export default function createPomodoro(
+  actions: any,
+  state: StoreType,
+  setState: SetStoreFunction<StoreType>
+) {
+  const [pomodoroState, setPomodoroState] =
+    createSignal<PomodoroFocusType>("Focus");
+  const selectedPomodoro = localStorage.getItem("activePomodoro");
+  const [activePomodoro, setActivePomodoro] = createSignal<PomodoroType | null>(
+    selectedPomodoro !== null ? JSON.parse(selectedPomodoro) : null
+  );
+
+  Object.assign<Actions, PomodoroActions>(actions, {
+    changePomodoroStatus(value: PomodoroFocusType) {
+      setPomodoroState(value);
+    },
+    editPomodoro() {
+      return editPomodoro(
+        {
+          id: activePomodoro()?._id,
+          est: activePomodoro()?.end,
+          // FIXME : fix the type (it should not be (as ...))
+          // should infer the type
+          act: (activePomodoro() as PomodoroType).current + 1,
+        },
+        state.token
+      );
+    },
+    changeActivePomodoro(activeItem: PomodoroType | null) {
+      setActivePomodoro(activeItem);
+      activeItem
+        ? localStorage.setItem(
+            "activePomodoro",
+            JSON.stringify(activePomodoro())
+          )
+        : localStorage.removeItem("activePomodoro");
+    },
+    cycleFocus() {
+      localStorage.setItem("activePomodoro", JSON.stringify(activePomodoro()));
+      setActivePomodoro((prev) => {
+        if (prev) {
+          return { ...prev, current: prev.current + 1 };
+        }
+        return prev;
+      });
+      localStorage.setItem("activePomodoro", JSON.stringify(activePomodoro()));
+    },
+  });
+
+  return {
+    pomodoroState,
+    activePomodoro,
+  };
+}
