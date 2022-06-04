091136a83fea70ceb3f3a44b0e40762374dc3a1d
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-04 22:58:12 +0430

    feat: pomodors resource

---
+++ src/store/createPomodoros.ts
@@ -0,0 +1,111 @@
+import { createResource } from "solid-js";
+import type { Resource } from "solid-js";
+import { SetStoreFunction } from "solid-js/store";
+import { StoreType } from "../types/store";
+import { BacklogType, PomodoroType } from "../types/pomodoro";
+import { fetchPomodoros, makeTaskDone } from "../api/pomodoros";
+import { changeOrder, changeTaskStatus, removePomodoro } from "../api/shared";
+import { Actions } from ".";
+import { ChangeOrderDto, ChangeTaskStatusDto } from "../types/shared";
+
+// TODO
+export interface PomodoroActions {
+  ////
+}
+
+export default function createPomodoros(
+  actions: any,
+  state: StoreType,
+  setState: SetStoreFunction<StoreType>
+): Resource<PomodoroType[]> {
+  const [pomodoros, { mutate, refetch }] = createResource<PomodoroType[]>(
+    async () => await fetchPomodoros(state.token),
+    {
+      initialValue: [],
+    }
+  );
+
+  Object.assign<Actions, PomodoroActions>(actions, {
+    loadPomodoros() {
+      return refetch();
+    },
+
+    mutatePomodoros(data: PomodoroType[]) {
+      return mutate(data);
+    },
+
+    remove(id: string) {
+      return removePomodoro(id, state.token);
+    },
+
+    clientRemove(id: string) {
+      mutate((prev) => {
+        return prev?.filter((item) => item._id !== id);
+      });
+    },
+
+    clientRemoveRevalidate(removedItem: PomodoroType) {
+      mutate((prev) => {
+        return [...prev, removedItem];
+      });
+    },
+
+    changeOrder(data: ChangeOrderDto) {
+      return changeOrder(data, state.token);
+    },
+
+    makeTaskDone(id: string) {
+      return makeTaskDone(id, state.token);
+    },
+
+    clientFilterDoneTask(id: string) {
+      mutate((prev) => {
+        return prev.map((item) => {
+          if (item._id === id) {
+            return {
+              ...item,
+              done: !item.done,
+            };
+          }
+          return item;
+        });
+      });
+    },
+
+    clientFilterDoneTaskRevalidate(id: string) {
+      mutate((prev) => {
+        return prev.map((item) => {
+          if (item._id === id) {
+            return {
+              ...item,
+              done: !item.done,
+            };
+          }
+          return item;
+        });
+      });
+    },
+
+    moveItemAndOrderItems(id: string) {
+      mutate((prev) => {
+        return prev
+          ?.filter((item) => item._id !== id)
+          .map((item, order) => ({ ...item, order: order + 1 }));
+      });
+    },
+
+    addMovedBacklogItem(movedItem: BacklogType) {
+      mutate((prev) => {
+        return [
+          ...prev,
+          { ...movedItem, status: "Pomodoro", order: prev.length },
+        ];
+      });
+    },
+
+    changeTaskStatus(id: string, data: ChangeTaskStatusDto) {
+      return changeTaskStatus(id, data, state.token);
+    },
+  });
+  return pomodoros;
+}
--- src/types/pomodoro.ts
+++ src/types/pomodoro.ts
@@ -1,19 +1,27 @@
-export type PomodoroType = {
+export type TaskType = {
   _id: string;
   title: string;
   current: number;
   end: number;
   done: boolean;
-  status: "Backlog" | "Pomodoro";
   order: number;
   created_at: string;
   updated_at: string;
 };
 
+export type BacklogType = TaskType & { status: "Backlog" };
+export type PomodoroType = TaskType & { status: "Pomodoro" };
+
 export type PomodoroFocusType = "Focus" | "Rest";
 export type PomodoroTimerState = "PLAY" | "PAUSE";
 
-export type CreatePomodoroItemDto = {
+export type CreateDto = {
   title: string;
   order: number;
+};
+
+export type EditPomodoroDto = {
+  id?: string;
+  est?: number;
+  act?: number;
 };
