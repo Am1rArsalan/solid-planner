b27c11ac163a9eabb1090cdfad07a90bb41a4e40
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-11 20:32:50 +0430

    feat: add button and icon button components

---
+++ src/components/UI/button/IconButton.tsx
@@ -0,0 +1,27 @@
+import { Component, mergeProps, splitProps } from "solid-js";
+import styles from "./Button.module.css";
+import { classNames } from "../utils/classNames";
+import { ElementType, HtmlProps } from "../../../types/ui";
+
+export type IconButtonProps<C extends ElementType = "button"> = HtmlProps<C>;
+
+export const IconButton: Component<IconButtonProps> = (props) => {
+  const { children } = props;
+
+  const propsWithDefault: IconButtonProps<"button"> = mergeProps(props);
+  const [local, _, others] = splitProps(
+    propsWithDefault,
+    ["class", "disabled"],
+    ["children"]
+  );
+
+  const classes = () => {
+    return classNames(styles.IconButton, local.class);
+  };
+
+  return (
+    <button class={classes()} {...others}>
+      {children}
+    </button>
+  );
+};
