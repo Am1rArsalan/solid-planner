4f0e768d373d05e6809a7ff55fdbb455f8d98843
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-11 08:54:55 +0430

    fix: move add api call to backlogs container

--- src/components/Backlogs.tsx
+++ src/components/Backlogs.tsx
@@ -1,6 +1,5 @@
 import { Component, createSignal, For } from "solid-js";
 import { BacklogsContainer, BacklogItem, SortableBacklogItem } from "./Backlog";
-import { PomodoroType } from "../types/pomodoro";
 import {
   DragDropProvider,
   DragDropSensors,
@@ -10,33 +9,15 @@ import {
 } from "@thisbeyond/solid-dnd";
 import { useStore } from "../store";
 
-const Backlogs: Component<{
+type Props = {
   move: (id: string, currentStatus: "Backlog" | "Pomodoro") => void;
-}> = ({ move }) => {
-  const [{ backlogs }, actions] = useStore();
+};
+
+const Backlogs: Component<Props> = ({ move }) => {
+  const [{ backlogs }, { mutateBacklogs, removeBacklog, changeOrder }] =
+    useStore();
   const [activeItem, setActiveItem] = createSignal(null);
   const backlogIds = () => backlogs().map((item) => item._id);
-
-  const handleAdd = async (task: string) => {
-    if (!task.length) return;
-    const creationTime = new Date().toISOString();
-
-    actions.addPendingItem({
-      creationTime,
-      title: task,
-    });
-
-    try {
-      const addedTask = await actions.addBacklog({
-        title: task,
-        order: backlogs().length,
-        end: 0,
-      });
-      actions.revalidateAddedItem(addedTask, creationTime);
-    } catch (error) {
-      actions.removePendingItem(creationTime);
-    }
-  };
 
   const handleRemoveBacklog = async (id: string) => {
     if (id.length === 0) return;
@@ -48,61 +29,60 @@ const Backlogs: Component<{
 
     let removedItem = backlogs()[removedItemIndex];
 
-    actions.mutateBacklogs(backlogs()?.filter((item) => item._id !== id));
+    mutateBacklogs(backlogs().filter((item) => item._id !== id));
     try {
-      await actions.removeBacklog(id);
+      await removeBacklog(id);
     } catch (error) {
-      actions.mutateBacklogs([...backlogs(), removedItem]);
+      mutateBacklogs([...backlogs(), removedItem]);
     }
   };
 
+  const onDragEnd = async ({ draggable, droppable }) => {
+    if (draggable && droppable) {
+      const currentItems = backlogs();
+      const fromIndex = currentItems.findIndex(
+        (item) => `${item._id}-${item.order}` === draggable.id
+      );
+      const toIndex = currentItems.findIndex(
+        (item) => `${item._id}-${item.order}` === droppable?.id
+      );
+      if (
+        currentItems[fromIndex]._id.includes("Pending") ||
+        currentItems[toIndex]._id.includes("Pending")
+      ) {
+        return;
+      }
+      if (fromIndex !== toIndex) {
+        let updatedItems = currentItems.slice();
+        updatedItems.splice(toIndex, 0, ...updatedItems.splice(fromIndex, 1));
+        mutateBacklogs(updatedItems);
+        try {
+          await changeOrder({
+            orders: updatedItems.map((item, order) => ({
+              ...item,
+              order: order + 1,
+            })),
+            status: "Backlog",
+          });
+        } catch {
+          // handle error
+        }
+      }
+    }
+    setActiveItem(null);
+  };
+
+  // FIX : ts error on DragDropProvider
   return (
     <DragDropProvider
       onDragStart={({ draggable }) => {
         setActiveItem(draggable);
+        return;
       }}
-      onDragEnd={async ({ draggable, droppable }) => {
-        if (draggable && droppable) {
-          const currentItems = backlogs();
-          const fromIndex = currentItems.findIndex(
-            (item) => `${item._id}-${item.order}` === draggable.id
-          );
-          const toIndex = currentItems.findIndex(
-            (item) => `${item._id}-${item.order}` === droppable?.id
-          );
-          if (
-            currentItems[fromIndex]._id.includes("Pending") ||
-            currentItems[toIndex]._id.includes("Pending")
-          ) {
-            return;
-          }
-          if (fromIndex !== toIndex) {
-            let updatedItems = currentItems.slice();
-            updatedItems.splice(
-              toIndex,
-              0,
-              ...updatedItems.splice(fromIndex, 1)
-            );
-
-            actions.mutateBacklogs(updatedItems);
-            try {
-              await actions.changeOrder({
-                orders: updatedItems.map((item, order) => ({
-                  ...item,
-                  order: order + 1,
-                })),
-                status: "Backlog",
-              });
-            } catch {
-              // handle error
-            }
-          }
-        }
-        setActiveItem(null);
-      }}
+      onDragEnd={onDragEnd}
       collisionDetector={closestCenter}
     >
-      <BacklogsContainer handleAdd={handleAdd}>
+      <BacklogsContainer>
         <div style={{ height: "3rem", width: "100%", padding: "1rem" }}>
           {backlogs.loading && "loading..."}
         </div>
@@ -112,7 +92,7 @@ const Backlogs: Component<{
             {(task) => (
               <SortableBacklogItem
                 backlog={task}
-                draggableId={`backlogs-dnd-${task._id}`}
+                draggableId={`backlogs()-dnd-${task._id}`}
               >
                 <button
                   disabled={task._id === "Pending"}
@@ -131,7 +111,7 @@ const Backlogs: Component<{
           </For>
         </SortableProvider>
         <DragOverlay>
-          <BacklogItem backlog={activeItem()} draggableId={"backlogs-dnd"}>
+          <BacklogItem backlog={activeItem()} draggableId={"backlogs()-dnd"}>
             <button>{"➡"}</button>
             <button>{"X"}</button>
           </BacklogItem>
