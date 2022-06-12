f0dd9e66e1c0308f77079d90fef9c27c14a950f3
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-12 18:30:43 +0430

    feat: add routes wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,16 @@ import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
 import { Provider } from "./store/";
+import { Router, Route, Routes } from "solid-app-router";
 
 render(
   () => (
     <Provider>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   ),
   document.getElementById("root") as HTMLElement
