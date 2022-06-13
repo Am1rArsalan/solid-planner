3dbe367f2fcaf5784d16caa2bd76fe5099371fb1
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-13 14:50:59 +0430

    feat: add routes

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,36 @@ import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
 import { Provider } from "./store/";
-import { Router, Route, Routes } from "solid-app-router";
+import { Router, Route, Routes, useNavigate } from "solid-app-router";
+import Auth from "./screens/Auth";
+
+const AppWithAuth = () => {
+  const nav = useNavigate();
+  const queryParams = new URLSearchParams(location.search);
+  if (queryParams.get("token")) {
+    localStorage.setItem("token", queryParams.get("token") as string);
+    const newUrl = `${window.location.pathname}`;
+    history.pushState({ path: newUrl }, "", newUrl);
+  }
+
+  const token = localStorage.getItem("token");
+  if (!token?.length) {
+    nav("/auth");
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  );
+};
 
 render(
   () => (
     <Provider>
       <Router>
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
+        <AppWithAuth />
       </Router>
     </Provider>
   ),
