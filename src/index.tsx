ae55325865d824247933f51921a9f84f7346e7c8
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-16 20:28:49 +0430

    chore: move localStorage token setter to store

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,14 @@ import { render } from "solid-js/web";
 
 import "./index.css";
 import App from "./App";
-import { Provider } from "./store/";
+import { Provider, useStore } from "./store/";
 import { Router, Route, Routes, useNavigate } from "solid-app-router";
 import Auth from "./screens/Auth";
 
 const AppWithAuth = () => {
   const nav = useNavigate();
-  const queryParams = new URLSearchParams(location.search);
-  if (queryParams.get("token")) {
-    localStorage.setItem("token", queryParams.get("token") as string);
-    const newUrl = `${window.location.pathname}`;
-    history.pushState({ path: newUrl }, "", newUrl);
-  }
+  const [{ token }] = useStore();
 
-  const token = localStorage.getItem("token");
   if (!token?.length) {
     nav("/auth");
   }
